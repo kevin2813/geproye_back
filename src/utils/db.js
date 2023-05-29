4c860061b02963201283ec6494dfce990430d5df
kevin2813 <kevin.rodast.95@gmail.com>
2023-05-29 00:15:22 -0400

    ahora se puede conectar a la BD secundaria

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,10 +2,24 @@ const { createClient } = require('@supabase/supabase-js');
 const config = require('../config/config');
 const logger = require('../config/logger');
 
-const supabase = createClient(config.supabase.url, config.supabase.key);
+const sb = [
+  [config.supabase.url, config.supabase.key],
+  [config.supabase.url2, config.supabase.key2]
+]
 
-if (supabase) {
-  logger.info('Connected to Supabase');
-} else logger.error('Error on supabase connect');
-
-module.exports = supabase;
+module.exports =  (async function (){
+  var i = 0;
+  var supabase = createClient(sb[i][0], sb[i][1]);
+  response = await supabase.from('proyectos').select().limit(1);
+  while(response.error) {
+    logger.error('Error al conectar a Supabase %d', i+1);
+    i = (i+1) % 2;
+    logger.warn('Intentando conectar a Supabase %d', i+1);
+    await new Promise(r => setTimeout(r, 2000));
+    supabase = createClient(sb[i][0], sb[i][1]);
+    response = await supabase.from('proyectos').select().limit(1);
+  }
+  
+  logger.info('Conectado a Supabase %d', i+1);
+  return supabase;
+})();
